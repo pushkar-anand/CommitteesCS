2c014d63dd6f72b86418e650e70d02c61d3c06c1
sureshn04 <sureshsunsuri@gmail.com>
2021-01-21 13:43:25 +0530

    added CSV func in students

--- student/handler.go
+++ student/handler.go
@@ -4,6 +4,7 @@ import (
 	"committees/helpers"
 	"committees/request"
 	"committees/template"
+	"encoding/csv"
 	"github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -48,4 +49,45 @@ func (h *Handler) Add(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, "/dashboard/students", http.StatusSeeOther)
+}
+
+func derefString(s *string) string {
+	if s != nil {
+		return *s
+	}
+
+	return ""
+}
+
+// Pushker just call me when you read this comment
+func (h *Handler) Csv(w http.ResponseWriter, r *http.Request) {
+	students, err := h.repository.FetchAll()
+	if err != nil {
+		h.logger.WithError(err).Error("error fetching students from DB")
+		helpers.InternalError(w)
+		return
+	}
+
+	data := make([][]string, 0)
+
+	data = append(data, []string{"Name", "Email", "Phone", "USN"})
+
+	for _, s :=range students {
+		d := make([]string, 0)
+		d = append(d, derefString(s.Name))
+		d = append(d, derefString(s.Email))
+		d = append(d, derefString(s.Phone))
+		d = append(d, derefString(s.Usn))
+
+		data = append(data, d)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=TheCSVFileName.csv")
+	wr := csv.NewWriter(w)
+	err = wr.WriteAll(data)
+	if err != nil {
+		http.Error(w, "Error sending csv: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
