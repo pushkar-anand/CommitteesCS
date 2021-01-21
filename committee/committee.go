ee381eaa254d53a7e90cb234ab0a20ddf18bcb4a
Pushkar Anand <anandpushkar088@gmail.com>
2021-01-21 18:02:09 +0530

    :bug: this committee creation

--- committee/committee.go
+++ committee/committee.go
@@ -8,10 +8,10 @@ import (
 
 type Committee struct {
 	model.Model
-	Name        *string            `gorm:"name" schema:"name,required" validate:"required,trim,printascii" json:"name"`
-	Description *string            `gorm:"description" schema:"description,required" validate:"required,trim,printascii" json:"description"`
-	Created     time.Time          `gorm:"creation_date" schema:"creation_date,required" validate:"required,trim,printascii" json:"creation_date"`
-	Members     []*faculty.Faculty `gorm:"many2many:committee_members;" schema:"members,required" json:"members"`
+	Name         *string            `gorm:"name" schema:"name,required" validate:"required,trim,printascii" json:"name"`
+	Description  *string            `gorm:"description" schema:"description,required" validate:"required,trim,printascii" json:"description"`
+	CreationDate time.Time          `gorm:"creation_date" schema:"creation_date,required" validate:"required,trim,printascii" json:"creation_date"`
+	Members      []*faculty.Faculty `gorm:"many2many:committee_members;" schema:"members,required" json:"members"`
 }
 
 type Members struct {
